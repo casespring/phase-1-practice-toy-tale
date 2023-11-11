077c0ebece4918e5c55ff772979ad8942dd932c3
Marco Upia <markremembers@gmail.com>
2023-11-11 16:48:34 -0500

    solved execpt if I add a new toy the likes doesn't update untill I refresh

--- src/index.js
+++ src/index.js
@@ -18,19 +18,71 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
           console.log(data)
           data.forEach(renderToys);
-        })
+
+        let newToyform = document.querySelector(".add-toy-form")
+        newToyform.addEventListener("submit", e => {
+          e.preventDefault();
+          
+          let newToynow = {
+            name: e.target.name.value,
+            image: e.target.image.value,
+            likes: 0,
+          };
+          (fetch("http://localhost:3000/toys", {
+            method: "POST",
+            headers: {
+              "content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: JSON.stringify({
+              name: e.target.name.value,
+              image: e.target.image.value,
+              likes: 0,
+            })
+          }))
+          .then(res => res.json())
+          .then(data => { 
+            console.log(data)
+          })
+          renderToys(newToynow);
+        });
+      });
+  
+        
 });
 
 
 function renderToys(toys) {
   let divCollection = document.querySelector("#toy-collection");
+  let divCard = document.createElement("div")
+  divCard.className = "card";
   let toyName = document.createElement('h2');
   toyName.textContent = toys.name;
   let toyImg = document.createElement('img');
+  toyImg.className = "toy-avatar"
   toyImg.src = toys.image;
   let  toyLikes = document.createElement('p');
   toyLikes.textContent = `${toys.likes} Likes`;
   let toyBtn = document.createElement('button');
-  toyBtn.className
+  toyBtn.textContent = "Like";
+  toyBtn.className = "like-btn";
+  divCard.append(toyName, toyImg, toyLikes, toyBtn);
+  divCollection.appendChild(divCard);
+  let counter = toys.likes;
 
+  toyBtn.addEventListener("click", e => {
+    counter++ 
+    toyLikes.textContent = `${counter} Likes`
+    fetch("http://localhost:3000/toys/" + toys.id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        likes: counter
+      })
+    })
+  })
 }
+
